d511499959fe1775fe343665c66b83b09a15ec2f
ismayilzeynal <ismayilbz@code.edu.az>
2022-08-11 15:23:30 +0400

    Encapsulation

---
+++ Encapsulation/Encapsulation/Models/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation.Models
+{
+    class User
+    {
+        //fields
+        private string _username;
+        private string _password;
+
+
+
+        // properties
+        public string Username { 
+            get 
+            {
+                return _username;
+            }
+            set 
+            {
+                if (value.Length > 5 && value.Length < 26)
+                    _username = value;
+            } 
+        }
+
+        public string Password {
+            get 
+            {
+                return _password;
+            }
+            set 
+            {
+                if (HasDigit(value) && HasUpper(value) && HasLower(value))
+                    _password = value;
+            }
+        }
+
+
+
+        //methods
+        private bool HasDigit(string Word)
+        {
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] > 47 && Word[i] < 58)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HasUpper(string Word)
+        {
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] > 64 && Word[i] < 91)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool HasLower(string Word)
+        {
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] > 96 && Word[i] < 123)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using Encapsulation.Models;
+using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User User1 = new User();
+            Console.Write("Username: ");
+            User1.Username=Console.ReadLine();
+            Console.Write("Password: ");
+            User1.Password=Console.ReadLine();
+        }
+    }
+}
